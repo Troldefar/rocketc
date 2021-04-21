b505e3585f6990c044879fa6f9d5ad587a7018c7
Rasmus Hansen <raha@cadpeople.dk>
2021-04-21 23:54:19 +0200

    setup

---
+++ src/components/layout/Main.vue
@@ -0,0 +1,17 @@
+<template>
+  <main class="main__container">
+    <div class="inner__nav">
+      Inner
+    </div>
+    <router-view class="inner__content" />
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+  setup() {
+    
+  },
+}
+</script>
